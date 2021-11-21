4ba87fa7de9e34d11494682ba7648a8fd7eb5062
Gerardo Lozano <eselgera@gmail.com>
2021-11-20 21:10:07 -0600

    Base de datos prueba

--- Connection.vb
+++ Connection.vb
@@ -5,28 +5,27 @@ Imports System.Data.SqlClient
 Module Connection
 
 
-   'Public ConexionSQL As SqlConnection
+    'Public ConexionSQL As SqlConnection
 
     Public sql As String
-        Public cmd As OdbcCommand
-        Public dr As OdbcDataReader
-        Public con As OdbcConnection
-        Public adapt As OdbcDataAdapter
-        'Public adapt As New SqlDataAdapter
-        Public reg As New DataSet
+    Public cmd As OdbcCommand
+    Public dr As OdbcDataReader
+    Public con As OdbcConnection
+    Public adapt As OdbcDataAdapter
+    'Public adapt As New SqlDataAdapter
+    Public reg As New DataSet
 
 
-        Sub connection_sql()
-            Try
+    Sub connection_sql()
+        Try
             con = New OdbcConnection("DSN=ProyectoDB;")
             If con.State = ConnectionState.Closed Then
-                    con.Open()
-                    MsgBox("Conexión exitosa")
-                End If
-            Catch ex As Exception
-                MsgBox("Error al tratar de conectar a la base de datos: " & ex.Message)
-            End Try
-        End Sub
-    End Module
-
+                con.Open()
+                MsgBox("Conexión exitosa")
+            End If
+        Catch ex As Exception
+            MsgBox("Error al tratar de conectar a la base de datos: " & ex.Message)
+        End Try
+    End Sub
 End Module
+
