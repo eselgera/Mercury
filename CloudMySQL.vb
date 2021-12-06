b9038f8d138e8b2384a0c2bd23aabcda20df6fbf
Gerardo Lozano <65347154+Gerardo-Lozano@users.noreply.github.com>
2021-12-06 09:16:21 -0600

    Se agrego múdulo de conexión a MY SQL Cloud y se comento el código en del módulo MySqlConnection para evitar problemas.

---
+++ CloudMySQL.vb
@@ -0,0 +1,27 @@
+﻿Imports MySql.Data.MySqlClient
+Module CloudMySQL
+    Public ConexionaJDESQL As MySqlConnection
+
+    Private _connection As New MySqlConnection
+    Private _errormessge As String
+    Public ReadOnly Property ErrorMessage() As String
+        Get
+            Return _errormessge
+        End Get
+    End Property
+    Public Function Connection() As Boolean
+        Try
+            _connection.connectionString = "Server=b8hrt3nyyisdb1zuqz9i-mysql.services.clever-cloud.com;Port=3306;Database=b8hrt3nyyisdb1zuqz9i;User ID=ufy3iskn6mnef5us;Password=zdaakkP7DXXiVmu7X411"
+            _connection.Open()
+            If _connection.State = ConnectionState.Open Then
+                MsgBox("Conexión realizada con exito")
+                _connection.Close()
+                Return True
+            End If
+        Catch ex As Exception
+            _errormessge = ex.Message
+            MsgBox("Sin éxito :( " & ex.Message)
+            Return False
+        End Try
+    End Function
+End Module
--- MySqlConnection.vb
+++ MySqlConnection.vb
@@ -1,13 +1,13 @@
-﻿Imports System.Data.SqlClient
+﻿'Imports System.Data.SqlClient
 
-Friend Class MySqlConnection
-    Private connectionString As Object
+'Friend Class MySqlConnection
+'    Private connectionString As Object
 
-    Public Sub New(connectionString As Object)
-        Me.connectionString = connectionString
-    End Sub
+'    Public Sub New(connectionString As Object)
+'        Me.connectionString = connectionString
+'    End Sub
 
-    Public Shared Widening Operator CType(v As MySqlConnection) As SqlConnection
-        Throw New NotImplementedException()
-    End Operator
-End Class
+'    Public Shared Widening Operator CType(v As MySqlConnection) As SqlConnection
+'        Throw New NotImplementedException()
+'    End Operator
+'End Class
