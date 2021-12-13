a83bd319a9c3b6b570a36fc1736000cafc1e8c17
Pablo Araoz Jiménez <ing.pabloaraoz@gmail.com>
2021-12-13 02:58:38 -0600

    Aplicando bono a empleado que mas vendio en el mes

--- EmpleadosForm.Designer.vb
+++ EmpleadosForm.Designer.vb
@@ -48,6 +48,9 @@ Partial Class EmpleadosForm
         Me.cbRol = New System.Windows.Forms.ComboBox()
         Me.txtCorreoEmpleado = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSueldo = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.btnBONO = New System.Windows.Forms.Button()
         CType(Me.TableEmpleados, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
@@ -55,7 +58,7 @@ Partial Class EmpleadosForm
         'TableEmpleados
         '
         Me.TableEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.TableEmpleados.Location = New System.Drawing.Point(10, 220)
+        Me.TableEmpleados.Location = New System.Drawing.Point(10, 233)
         Me.TableEmpleados.Margin = New System.Windows.Forms.Padding(2)
         Me.TableEmpleados.MultiSelect = False
         Me.TableEmpleados.Name = "TableEmpleados"
@@ -67,9 +70,9 @@ Partial Class EmpleadosForm
         '
         'btnSearchEmpleado
         '
-        Me.btnSearchEmpleado.Location = New System.Drawing.Point(508, 185)
+        Me.btnSearchEmpleado.Location = New System.Drawing.Point(495, 207)
         Me.btnSearchEmpleado.Name = "btnSearchEmpleado"
-        Me.btnSearchEmpleado.Size = New System.Drawing.Size(101, 25)
+        Me.btnSearchEmpleado.Size = New System.Drawing.Size(92, 22)
         Me.btnSearchEmpleado.TabIndex = 20
         Me.btnSearchEmpleado.Text = "Buscar"
         Me.btnSearchEmpleado.UseVisualStyleBackColor = True
@@ -79,15 +82,15 @@ Partial Class EmpleadosForm
         Me.GroupBox2.Controls.Add(Me.btnUpdateEmpleado)
         Me.GroupBox2.Controls.Add(Me.btnAgregarEmpleado)
         Me.GroupBox2.Controls.Add(Me.btnNewEmpleado)
-        Me.GroupBox2.Location = New System.Drawing.Point(620, 34)
+        Me.GroupBox2.Location = New System.Drawing.Point(620, 14)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(156, 176)
+        Me.GroupBox2.Size = New System.Drawing.Size(156, 131)
         Me.GroupBox2.TabIndex = 19
         Me.GroupBox2.TabStop = False
         '
         'btnUpdateEmpleado
         '
-        Me.btnUpdateEmpleado.Location = New System.Drawing.Point(9, 109)
+        Me.btnUpdateEmpleado.Location = New System.Drawing.Point(9, 90)
         Me.btnUpdateEmpleado.Name = "btnUpdateEmpleado"
         Me.btnUpdateEmpleado.Size = New System.Drawing.Size(141, 30)
         Me.btnUpdateEmpleado.TabIndex = 2
@@ -96,7 +99,7 @@ Partial Class EmpleadosForm
         '
         'btnAgregarEmpleado
         '
-        Me.btnAgregarEmpleado.Location = New System.Drawing.Point(9, 72)
+        Me.btnAgregarEmpleado.Location = New System.Drawing.Point(9, 53)
         Me.btnAgregarEmpleado.Name = "btnAgregarEmpleado"
         Me.btnAgregarEmpleado.Size = New System.Drawing.Size(141, 30)
         Me.btnAgregarEmpleado.TabIndex = 1
@@ -105,7 +108,7 @@ Partial Class EmpleadosForm
         '
         'btnNewEmpleado
         '
-        Me.btnNewEmpleado.Location = New System.Drawing.Point(9, 36)
+        Me.btnNewEmpleado.Location = New System.Drawing.Point(9, 17)
         Me.btnNewEmpleado.Name = "btnNewEmpleado"
         Me.btnNewEmpleado.Size = New System.Drawing.Size(141, 30)
         Me.btnNewEmpleado.TabIndex = 0
@@ -115,7 +118,7 @@ Partial Class EmpleadosForm
         'txtBuscarEmpleado
         '
         Me.txtBuscarEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtBuscarEmpleado.Location = New System.Drawing.Point(64, 188)
+        Me.txtBuscarEmpleado.Location = New System.Drawing.Point(64, 207)
         Me.txtBuscarEmpleado.Name = "txtBuscarEmpleado"
         Me.txtBuscarEmpleado.Size = New System.Drawing.Size(425, 22)
         Me.txtBuscarEmpleado.TabIndex = 40
@@ -125,7 +128,7 @@ Partial Class EmpleadosForm
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.Color.Black
-        Me.Label6.Location = New System.Drawing.Point(8, 190)
+        Me.Label6.Location = New System.Drawing.Point(8, 209)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(50, 16)
         Me.Label6.TabIndex = 17
@@ -135,7 +138,7 @@ Partial Class EmpleadosForm
         '
         Me.txtidEmpleado.Enabled = False
         Me.txtidEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtidEmpleado.Location = New System.Drawing.Point(82, 40)
+        Me.txtidEmpleado.Location = New System.Drawing.Point(77, 12)
         Me.txtidEmpleado.Name = "txtidEmpleado"
         Me.txtidEmpleado.ReadOnly = True
         Me.txtidEmpleado.Size = New System.Drawing.Size(80, 22)
@@ -146,7 +149,7 @@ Partial Class EmpleadosForm
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label7.ForeColor = System.Drawing.Color.Black
-        Me.Label7.Location = New System.Drawing.Point(25, 40)
+        Me.Label7.Location = New System.Drawing.Point(20, 12)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(21, 16)
         Me.Label7.TabIndex = 31
@@ -155,7 +158,7 @@ Partial Class EmpleadosForm
         'txtUserEmpleado
         '
         Me.txtUserEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtUserEmpleado.Location = New System.Drawing.Point(440, 62)
+        Me.txtUserEmpleado.Location = New System.Drawing.Point(435, 34)
         Me.txtUserEmpleado.Name = "txtUserEmpleado"
         Me.txtUserEmpleado.Size = New System.Drawing.Size(169, 22)
         Me.txtUserEmpleado.TabIndex = 25
@@ -165,7 +168,7 @@ Partial Class EmpleadosForm
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.Color.Black
-        Me.Label5.Location = New System.Drawing.Point(350, 67)
+        Me.Label5.Location = New System.Drawing.Point(345, 39)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(55, 16)
         Me.Label5.TabIndex = 30
@@ -174,7 +177,7 @@ Partial Class EmpleadosForm
         'txtTelefonoEmpleado
         '
         Me.txtTelefonoEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtTelefonoEmpleado.Location = New System.Drawing.Point(82, 121)
+        Me.txtTelefonoEmpleado.Location = New System.Drawing.Point(77, 93)
         Me.txtTelefonoEmpleado.Name = "txtTelefonoEmpleado"
         Me.txtTelefonoEmpleado.Size = New System.Drawing.Size(230, 22)
         Me.txtTelefonoEmpleado.TabIndex = 23
@@ -184,7 +187,7 @@ Partial Class EmpleadosForm
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.Black
-        Me.Label3.Location = New System.Drawing.Point(25, 126)
+        Me.Label3.Location = New System.Drawing.Point(20, 98)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(62, 16)
         Me.Label3.TabIndex = 28
@@ -193,7 +196,7 @@ Partial Class EmpleadosForm
         'txtApEmpleado
         '
         Me.txtApEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtApEmpleado.Location = New System.Drawing.Point(82, 93)
+        Me.txtApEmpleado.Location = New System.Drawing.Point(77, 65)
         Me.txtApEmpleado.Name = "txtApEmpleado"
         Me.txtApEmpleado.Size = New System.Drawing.Size(230, 22)
         Me.txtApEmpleado.TabIndex = 22
@@ -203,7 +206,7 @@ Partial Class EmpleadosForm
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.Black
-        Me.Label2.Location = New System.Drawing.Point(25, 96)
+        Me.Label2.Location = New System.Drawing.Point(20, 68)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(58, 16)
         Me.Label2.TabIndex = 26
@@ -212,7 +215,7 @@ Partial Class EmpleadosForm
         'txtNombreEmpleado
         '
         Me.txtNombreEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtNombreEmpleado.Location = New System.Drawing.Point(82, 64)
+        Me.txtNombreEmpleado.Location = New System.Drawing.Point(77, 36)
         Me.txtNombreEmpleado.Name = "txtNombreEmpleado"
         Me.txtNombreEmpleado.Size = New System.Drawing.Size(230, 22)
         Me.txtNombreEmpleado.TabIndex = 21
@@ -222,7 +225,7 @@ Partial Class EmpleadosForm
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(25, 69)
+        Me.Label1.Location = New System.Drawing.Point(20, 41)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(57, 16)
         Me.Label1.TabIndex = 23
@@ -231,7 +234,7 @@ Partial Class EmpleadosForm
         'txtPassEmpleado
         '
         Me.txtPassEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtPassEmpleado.Location = New System.Drawing.Point(440, 86)
+        Me.txtPassEmpleado.Location = New System.Drawing.Point(435, 58)
         Me.txtPassEmpleado.Name = "txtPassEmpleado"
         Me.txtPassEmpleado.Size = New System.Drawing.Size(169, 22)
         Me.txtPassEmpleado.TabIndex = 26
@@ -241,7 +244,7 @@ Partial Class EmpleadosForm
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label8.ForeColor = System.Drawing.Color.Black
-        Me.Label8.Location = New System.Drawing.Point(350, 91)
+        Me.Label8.Location = New System.Drawing.Point(345, 63)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(77, 16)
         Me.Label8.TabIndex = 34
@@ -252,7 +255,7 @@ Partial Class EmpleadosForm
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label9.ForeColor = System.Drawing.Color.Black
-        Me.Label9.Location = New System.Drawing.Point(350, 119)
+        Me.Label9.Location = New System.Drawing.Point(345, 91)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(29, 16)
         Me.Label9.TabIndex = 36
@@ -263,7 +266,7 @@ Partial Class EmpleadosForm
         Me.Label10.AutoSize = True
         Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label10.ForeColor = System.Drawing.Color.Black
-        Me.Label10.Location = New System.Drawing.Point(350, 145)
+        Me.Label10.Location = New System.Drawing.Point(345, 117)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(52, 16)
         Me.Label10.TabIndex = 35
@@ -274,7 +277,7 @@ Partial Class EmpleadosForm
         Me.cbEstatus.DisplayMember = "id"
         Me.cbEstatus.FormattingEnabled = True
         Me.cbEstatus.Items.AddRange(New Object() {"Baja", "Activo"})
-        Me.cbEstatus.Location = New System.Drawing.Point(440, 139)
+        Me.cbEstatus.Location = New System.Drawing.Point(435, 111)
         Me.cbEstatus.Margin = New System.Windows.Forms.Padding(2)
         Me.cbEstatus.Name = "cbEstatus"
         Me.cbEstatus.Size = New System.Drawing.Size(169, 21)
@@ -284,7 +287,7 @@ Partial Class EmpleadosForm
         'cbRol
         '
         Me.cbRol.FormattingEnabled = True
-        Me.cbRol.Location = New System.Drawing.Point(440, 113)
+        Me.cbRol.Location = New System.Drawing.Point(435, 85)
         Me.cbRol.Margin = New System.Windows.Forms.Padding(2)
         Me.cbRol.Name = "cbRol"
         Me.cbRol.Size = New System.Drawing.Size(169, 21)
@@ -293,7 +296,7 @@ Partial Class EmpleadosForm
         'txtCorreoEmpleado
         '
         Me.txtCorreoEmpleado.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCorreoEmpleado.Location = New System.Drawing.Point(82, 151)
+        Me.txtCorreoEmpleado.Location = New System.Drawing.Point(77, 123)
         Me.txtCorreoEmpleado.Name = "txtCorreoEmpleado"
         Me.txtCorreoEmpleado.Size = New System.Drawing.Size(230, 22)
         Me.txtCorreoEmpleado.TabIndex = 24
@@ -303,18 +306,51 @@ Partial Class EmpleadosForm
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.Black
-        Me.Label4.Location = New System.Drawing.Point(25, 156)
+        Me.Label4.Location = New System.Drawing.Point(20, 128)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(49, 16)
         Me.Label4.TabIndex = 40
         Me.Label4.Text = "Correo"
+        '
+        'txtSueldo
+        '
+        Me.txtSueldo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSueldo.Location = New System.Drawing.Point(77, 154)
+        Me.txtSueldo.Name = "txtSueldo"
+        Me.txtSueldo.Size = New System.Drawing.Size(230, 22)
+        Me.txtSueldo.TabIndex = 41
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.Black
+        Me.Label11.Location = New System.Drawing.Point(20, 157)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(51, 16)
+        Me.Label11.TabIndex = 42
+        Me.Label11.Text = "Sueldo"
+        '
+        'btnBONO
+        '
+        Me.btnBONO.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnBONO.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBONO.Location = New System.Drawing.Point(620, 157)
+        Me.btnBONO.Name = "btnBONO"
+        Me.btnBONO.Size = New System.Drawing.Size(156, 46)
+        Me.btnBONO.TabIndex = 43
+        Me.btnBONO.Text = "Aplicar Bono"
+        Me.btnBONO.UseVisualStyleBackColor = False
         '
         'EmpleadosForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(794, 419)
+        Me.ClientSize = New System.Drawing.Size(794, 437)
+        Me.Controls.Add(Me.btnBONO)
+        Me.Controls.Add(Me.txtSueldo)
+        Me.Controls.Add(Me.Label11)
         Me.Controls.Add(Me.txtCorreoEmpleado)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.cbRol)
@@ -374,4 +410,7 @@ Partial Class EmpleadosForm
     Friend WithEvents cbRol As ComboBox
     Friend WithEvents txtCorreoEmpleado As TextBox
     Friend WithEvents Label4 As Label
+    Friend WithEvents txtSueldo As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents btnBONO As Button
 End Class
